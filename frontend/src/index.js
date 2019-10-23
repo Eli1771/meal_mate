7fe55e6ab0141ae97d4b1a6320a4458e09173eca
github username <github email address>
2019-10-23 11:12:04 -0700

    Setting initial fetch statement, adding and configuring gems

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,4 +3,6 @@ const RECIPES_URL = `${BASE_URL}/recipes`;
 
 $(document).ready(function() {
   console.log('Hello World!');
+  fetch(RECIPES_URL).then(resp => resp.json())
+    .then(json => console.log(json));
 });
