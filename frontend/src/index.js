77b2059131ab633994b806c4abd36562bf7bd0b0
github username <github email address>
2019-10-28 08:04:12 -0700

    Stubbing event listeners, deleted unnecessarry divs in button map

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,13 @@ const RECIPES_URL = `${BASE_URL}/recipes`;
 
 $(document).ready(function() {
   generateMealButtons();
+  //fetchCurrentPlan(); <-grabs, if any, the current meal plan and displays todays plan
+  loadEventListeners();
 });
+
+
+
+//                        --------LOAD PAGE ELEMENTS-------
 
 
 
@@ -18,9 +24,22 @@ function generateMealButtons() {
       let customId = `checkbox-${i}-${j}`;
       buttons.innerHTML +=
         `<input type="checkbox" class="meal-button-checkbox" id="checkbox-${customId}">
-        <label for="checkbox-${customId}">
-        <div class="button-graphic"><div></div></div></label>`;
+        <label for="checkbox-${customId}"></label>`;
         //above are 2 nested divs to be used to display a retro button with some depth
     }
   }
+}
+
+//                        -------LOAD EVENT LISTENERS-------
+
+
+
+function loadEventListeners() {
+  console.log('hello world');
+  let generatePlanButton = document.querySelector('#oven button');
+  generatePlanButton.addEventListener('click', generatePlan);
+}
+
+function generatePlan() {
+  console.log('I\'m generating!');
 }
