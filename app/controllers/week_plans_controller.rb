33b914a26b3de160fcac864ce0f6babd701296ee
github username <github email address>
2019-11-06 19:37:48 -0800

    Set basic controller actions for week plan, need to update db

--- app/controllers/week_plans_controller.rb
+++ app/controllers/week_plans_controller.rb
@@ -1,2 +1,17 @@
 class WeekPlansController < ApplicationController
+  def index
+    week_plan = WeekPlan.find(params[:id])
+    render :json week_plan
+  end
+
+  def create
+    week_plan = WeekPlan.create(week_plan_params)
+    render :json week_plan
+  end
+
+  private
+
+  def week_plan_params
+    params.require(:week_plan).permit(:sunday, :monday, :tuesday, :wednesday, :thursday, :friday, :saturday);
+  end
 end
