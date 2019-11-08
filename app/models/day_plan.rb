d903772e317ae7ec046190764280e6b67f3ded5a
github username <github email address>
2019-11-08 14:12:29 -0800

    First custom getters and json rendering for day plan with testing

--- app/models/day_plan.rb
+++ app/models/day_plan.rb
@@ -1,5 +1,9 @@
 class DayPlan < ApplicationRecord
+  #custom setter/getter methods
 
+  def breakfast_recipe
+    Recipe.find(self.breakfast)
+  end
 
   def self.clearAll
     self.all.each do |dp|
