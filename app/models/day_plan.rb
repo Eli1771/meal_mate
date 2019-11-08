140f6357a1696af391a11f2bf48d8ed578ef7615
github username <github email address>
2019-11-08 14:27:45 -0800

    All custom getter methods for meal plans. Refactor later?

--- app/models/day_plan.rb
+++ app/models/day_plan.rb
@@ -4,6 +4,12 @@ class DayPlan < ApplicationRecord
   def breakfast_recipe
     Recipe.find(self.breakfast)
   end
+  def lunch_recipe
+    Recipe.find(self.lunch)
+  end
+  def dinner_recipe
+    Recipe.find(self.lunch)
+  end
 
   def self.clearAll
     self.all.each do |dp|
--- app/models/week_plan.rb
+++ app/models/week_plan.rb
@@ -1,2 +1,30 @@
 class WeekPlan < ApplicationRecord
+
+  def sunday_plan
+    DayPlan.find(self.sunday)
+  end
+  def monday_plan
+    DayPlan.find(self.monday)
+  end
+  def tuesday_plan
+    DayPlan.find(self.tuesday)
+  end
+  def wednesday_plan
+    DayPlan.find(self.wednesday)
+  end
+  def thursday_plan
+    DayPlan.find(self.thursday)
+  end
+  def friday_plan
+    DayPlan.find(self.friday)
+  end
+  def saturday_plan
+    DayPlan.find(self.saturday)
+  end
+
+  def self.clearAll
+    self.all.each do |wp|
+      wp.delete
+    end
+  end
 end
