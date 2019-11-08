e3266e185d3ddcd590e0a6b901c12b5aac002507
github username <github email address>
2019-11-08 10:43:22 -0800

    First controller actions to vew/create weekPlans

--- app/controllers/week_plans_controller.rb
+++ app/controllers/week_plans_controller.rb
@@ -1,2 +1,11 @@
 class WeekPlansController < ApplicationController
+  def index
+    week_plan = WeekPlan.find(params[:id])
+    render json: week_plan
+  end
+
+  def create
+    week_plan = WeekPlan.create(params)
+    render json: week_plan
+  end 
 end
