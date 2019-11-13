489e524d8565dee6b1ed4ecb2ba45e0e172eabdc
github username <github email address>
2019-11-12 17:05:36 -0800

    Create controller action and strong params for recipe_plans

--- app/controllers/recipe_plans_controller.rb
+++ app/controllers/recipe_plans_controller.rb
@@ -1,2 +1,12 @@
 class RecipePlansController < ApplicationController
+  def create
+    recipe_plan = RecipePlan.create(recipe_plan_params)
+    render json: recipe_plan
+  end
+
+  private
+
+  def recipe_plan_params
+    params.require(:recipe_plan).permit(:recipe_id, :day_plan_id)
+  end
 end
