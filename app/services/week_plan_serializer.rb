39877957401ff9b071cf28b94abb9fc8f3682239
github username <github email address>
2019-11-13 13:34:58 -0800

    Deleted old reference comment block

--- app/services/week_plan_serializer.rb
+++ app/services/week_plan_serializer.rb
@@ -17,18 +17,3 @@ class WeekPlanSerializer
     }, :except => [:id, :created_at, :updated_at]);
   end
 end
-
-# "#<NoMethodError: undefined method `except' for #<WeekPlan:0x00007fab1edd17f8>
-# Did you mean?  exec>"
-
-# @day_plan.to_json(:include => {
-#   :breakfast_recipe => {
-#     :include => {:ingredients => {:only => :name}},
-#     :only => :name},
-#   :lunch_recipe => {
-#     :include => {:ingredients => {:only => :name}},
-#     :only => :name},
-#   :dinner_recipe => {
-#     :include => {:ingredients => {:only => :name}},
-#     :only => :name},
-# }, :except => [:created_at, :updated_at])
