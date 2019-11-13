6430f33da0f475dbb138665b914f6bb5c7d49507
github username <github email address>
2019-11-13 13:32:14 -0800

    Working serializer for week_plans

--- app/services/week_plan_serializer.rb
+++ app/services/week_plan_serializer.rb
@@ -4,7 +4,17 @@ class WeekPlanSerializer
   end
 
   def to_serialized_json
-    
+    @week_plan.to_json(:include => {
+      :day_plans => {:include => {
+        :day => {:only => [:name]},
+        :recipes => {:include => {
+          :meal => {:only => [:name]},
+          :recipe_ingredients => {:include => {
+            :ingredient => {:only => [:name]}
+          }, :only => [:amount, :unit]}
+        }, :only => [:name, ]},
+      }, :only => [:date]}
+    }, :except => [:id, :created_at, :updated_at]);
   end
 end
 
