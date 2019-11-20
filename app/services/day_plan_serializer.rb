65e19fb57696280423e6a7916eaf4c0ae067de43
github username <github email address>
2019-11-20 12:43:07 -0800

    Reconfigured dp serializer to include ingredient elements

--- app/services/day_plan_serializer.rb
+++ app/services/day_plan_serializer.rb
@@ -6,7 +6,10 @@ class DayPlanSerializer
   def to_serialized_json
     @day_plan.to_json(:include => {
       :recipes => {:include => {
-        :meal => {:only => [:name]}},
+        :meal => {:only => [:name]},
+        :recipe_ingredients => {:include => {
+          :ingredient => {:only => [:name]}},
+          :only => [:amount, :unit]}},
         :only => [:name, :instructions]
       }
     }, :except => [:id, :day_id, :date, :week_plan_id, :created_at, :updated_at])
