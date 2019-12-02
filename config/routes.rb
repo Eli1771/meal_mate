61c3e67e7eb2143a7335512089c0f7148b78d1b6
github username <github email address>
2019-12-01 20:17:41 -0800

    Narrowing route options

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,10 @@
 Rails.application.routes.draw do
   resources :migrations
-  resources :recipe_plans
+  resources :recipe_plans, only: [:new, :create]
   resources :meals, only: [:show] do
     resources :recipes, only: [:index, :show]
   end
-  resources :days
-  resources :week_plans, param: :slug
-  resources :day_plans, param: :slug
-  resources :recipe_ingredients
-  resources :ingredients
-  resources :recipes
+  resources :week_plans, only: [:new, :create, :show], param: :slug
+  resources :day_plans, only: [:new, :create, :show], param: :slug
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
